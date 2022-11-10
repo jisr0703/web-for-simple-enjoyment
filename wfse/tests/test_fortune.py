c94d6fe2a204c5fbfea14b11a8ec86b542cc034e
seungryul <sungryul@stanworld.kr>
2022-11-11 00:40:10 +0900

    pytest DB insert, select 확인

--- wfse/tests/test_fortune.py
+++ wfse/tests/test_fortune.py
@@ -4,7 +4,8 @@ from wfse.app.main import create_app
 
 
 class TestCase:
-    ip: str = '192.168.0.12'
+    new_ip: str = '192.168.0.101'
+    exist_ip: str = '192.168.0.111'
 
 
 client = TestClient(create_app())
@@ -30,16 +31,32 @@ def test_fortune_checking():
     assert response.json() == {'message': 'here is fortune checking'}
 
 
-def test_fortune_result():
-    response = client.get(f'/fortune/result/{TestCase.ip}')
+def test_fortune_checking_testing():
+    print()
+    response = client.get(f'/fortune/checking/test?feeling_id={1}')
+    print(response)
     response_body = response.json()
-    print(f'\n{response_body}')
+    print(f'{response_body}')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'here is fortune checking'}
+
+
+def test_fortune_result():
+    response = client.get(f'/fortune/result/{TestCase.new_ip}')
+    response_body = response.json()
     assert response.status_code == 201
-    assert 'Feeling' in response_body.keys()
+    assert 'Feeling' in response_body.keys() and response_body['exist'] == False
+
+
+def test_fortune_exist_result():
+    response = client.get(f'/fortune/result/{TestCase.exist_ip}')
+    response_body = response.json()
+    assert response.status_code == 201
+    assert 'feeling' in response_body.keys() and response_body['exist'] == True
 
 
 def test_fortune_invalid_result():
     response = client.get('/fortune/result')
     response_body = response.json()
-    assert response.status_code == 400
+    assert response.status_code == 404
     assert 'INVALID_REQUEST' == response_body["msg"]
